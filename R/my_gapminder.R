016f1f5039aad8d43c31a2831da8c54f4eac8fb2
Josiah Thulin <thulinjt@gmail.com>
2021-03-17 11:55:13 -0700

    Add documented gapminder data as my_gapminder

---
+++ R/my_gapminder.R
@@ -0,0 +1,18 @@
+#' Gapminder data
+#'
+#' Excerpt of the Gapminder data on life expectancy, GDP per capita, and
+#' population by country.
+#' This data and documentation come from the \code{gapminder} package,
+#' available as \code{\link[gapminder]{gapminder}}.
+#'
+#' @format The main data frame \code{gapminder} has 1704 rows and 6 variables:
+#' \describe{
+#'   \item{country}{factor with 142 levels}
+#'   \item{continent}{factor with 5 levels}
+#'   \item{year}{ranges from 1952 to 2007 in increments of 5 years}
+#'   \item{lifeExp}{life expectancy at birth, in years}
+#'   \item{pop}{population}
+#'   \item{gdpPercap}{GDP per capita (US$, inflation-adjusted)}
+#' }
+#' @source \url{http://www.gapminder.org/data/}
+"my_gapminder"
